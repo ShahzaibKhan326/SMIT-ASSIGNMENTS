7ee15eb0ad5dcbac7e82a0a9c4f9cf49f7622b97
ShahzaibKhan326 <kshahzaib07@gmail.com>
2025-11-13 09:37:08 -0800

    add-responsive- operater button

--- Assignment-Salt_and_Pepper/js/scripts.js
+++ Assignment-Salt_and_Pepper/js/scripts.js
@@ -7,3 +7,39 @@ setInterval(() => {
    imageContainer.src = imageCollection[index];
 
 }, 3000);
+
+//--------
+   let contactDetail = document.getElementById("content-detail");
+   let contentList = document.getElementById("list-content");
+   let list_plusSign = document.getElementById("list_plus_icon");
+   let list_minusSign = document.getElementById("list_minus_icon");
+   let link_plusSign = document.getElementById("link_plus_icon");
+   let link_minusSign = document.getElementById("link_minus_icon");
+
+function showContacDetail ()
+{
+   contactDetail.style.display="block";
+   list_plusSign.style.display="none";
+   list_minusSign.style.display="block"
+
+}
+function hideContactDetail()
+{
+   contactDetail.style.display="none";
+   list_plusSign.style.display="block";
+   list_minusSign.style.display="none"
+}
+
+function showContentlinks()
+{
+   contentList.style.display="block";
+   link_plusSign.style.display="none";
+   link_minusSign.style.display="block";
+
+}
+function hideContentlinks()
+{
+   contentList.style.display="none";
+   link_plusSign.style.display="block";
+   link_minusSign.style.display="none";
+}
